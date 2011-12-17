66bfe4538ec619a4906a7eea53a423322747c836
lyhcode (Yan-hong Lin) <lyhcode@gmail.com>
2011-12-17 17:05:14 +0800

    initial

--- jquery.codemirror.js
+++ jquery.codemirror.js
@@ -1,13 +1,14 @@
-(function($){$.fn.codemirrorHighlight = function(options) {
-	
+(function($){$.fn.codemirror = function(options) {
+
+	var result = this;
+
 	var settings = $.extend( {
-      'mode' : 'text/x-rst',
-	  'gutter' : false
+      'mode' : 'javascript',
+	  'lineNumbers' : false,
+	  'runmode' : false
     }, options);
 
-	var result = null;
-	
-	this.each(function() {
+	if (settings.runmode) this.each(function() {
 		var obj = $(this);
 		var accum = [], gutter = [], size = 0;
 		var callback = function(string, style) {
@@ -23,10 +24,13 @@
 			}
 		}
 		CodeMirror.runMode(obj.val(), settings.mode, callback);
-		result = $('<div class="CodeMirror">'+(settings.gutter?('<div class="CodeMirror-gutter"><div class="CodeMirror-gutter-text">'+gutter.join('')+'</div></div>'):'<!--gutter-->')+'<div class="CodeMirror-lines">'+(settings.gutter?'<div style="position: relative; margin-left: '+size.toString().length+'em;">':'<div>')+'<pre class="cm-s-default">'+accum.join('')+'</pre></div></div></div>').insertAfter(obj);
+		$('<div class="CodeMirror">'+(settings.lineNumbers?('<div class="CodeMirror-gutter"><div class="CodeMirror-gutter-text">'+gutter.join('')+'</div></div>'):'<!--gutter-->')+'<div class="CodeMirror-lines">'+(settings.lineNumbers?'<div style="position: relative; margin-left: '+size.toString().length+'em;">':'<div>')+'<pre class="cm-s-default">'+accum.join('')+'</pre></div></div></div>').insertAfter(obj);
 		obj.hide();
 	});
+	else this.each(function() {
+		result = CodeMirror.fromTextArea(this, settings);
+	});
+
 	return result;
 };})( jQuery );
-
 
